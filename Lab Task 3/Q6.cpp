12f02298af4028dec71e936279e220ff0292092b
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-02-03 19:50:49 +0500

    Update Q6.cpp

--- Lab Task 3/Q6.cpp
+++ Lab Task 3/Q6.cpp
@@ -1,3 +1,19 @@
+/* Create a class called Smartphone with the following attributes:
+  --> Company
+  --> Model
+  --> Display Resolution
+  --> RAM
+  --> ROM
+  --> Storage
+Create getter and setter methods for your attributes. A smartphone has some specific actions that it can perform.
+For example:
+1. Make a phone call
+2. Send a message
+3. Connect to the wifi
+4. Browse the internet
+Create different smartphone objects. Set their attributes using the setter functions and display their attributes after
+using the getter functions to fetch the attributes. */
+
 #include <iostream>
 using namespace std;
 
