fa639653f0b29558c6c16521d79eb96f38bec7ce
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-03-03 20:36:50 +0500

    Update Q6.cpp

--- Lab Task 5/Q6.cpp
+++ Lab Task 5/Q6.cpp
@@ -1,3 +1,11 @@
+/*
+Write a program that uses composition to implement a game engine. A game engine is madeup of several components. For example:
+a) Graphics rendering engine
+b) Input handler
+c) Physics engine
+You don’t have to write the logicforhow these individual components work.
+*/
+
 #include <iostream>
 using namespace std;
 
