ef98f12ddd91b210658ed32cdd4a7efdbccf0fcb
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-03-17 21:26:48 +0500

    Update Q1.cpp

--- Lab Task 7/Q1.cpp
+++ Lab Task 7/Q1.cpp
@@ -12,9 +12,14 @@ public:
     Account(string num, double bal, string name, string type = "") : accountNumber(num), balance(bal), accountHolderName(name), accountType(type) {}
 
     virtual void deposit(double amount){
-        balance += amount;
-        cout << "The amount $" << amount << " has been deposited into your account." << endl;
-        cout << "Remaining balance: $" << balance << endl;
+        if(amount > 0){
+            balance += amount;
+            cout << "The amount $" << amount << " has been deposited into your account." << endl;
+            cout << "Remaining balance: $" << balance << endl;
+        }
+        else{
+            cout << "Invalid amount entered." << endl;
+        }
     }
 
     virtual void withdraw(double amount){
@@ -32,11 +37,11 @@ public:
     virtual void printStatement(){}  // Empty method, to be overriden in derived classes
 
     virtual void getAccountInfo(){
-        cout << "Displaying account information below" << endl;
+        cout << endl << "Displaying account information below" << endl;
         cout << "Account Number: " << accountNumber << endl;
         cout << "Account Balance: $" << balance << endl;
         cout << "Account Holder Name: " << accountHolderName << endl;
-        cout << "Account Type: " << ((accountType != "") ? accountType : "Optional") << endl;
+        cout << "Account Type: " << ((accountType != "") ? accountType : "-") << endl;
     }
 };
 
@@ -45,7 +50,7 @@ class SavingsAccount : public Account{
     double minimumBalance;
 
 public:
-    SavingsAccount(double rate, double minBal, string num, double bal, string name, string type = "") :  interestRate(rate), minimumBalance(minBal), Account(num, bal, name, type){
+    SavingsAccount(double rate, double minBal, string num, double bal, string name, string type = "Savings Account") :  interestRate(rate), minimumBalance(minBal), Account(num, bal, name, type){
         if(balance < minimumBalance){
             do{
                 cout << "The entered balance ($" << balance << ") is less than the minimum balance ($" << minimumBalance << "). Enter account balance again: ";
@@ -73,15 +78,16 @@ public:
     }
 
     void printStatement() override {
-        cout << "--------- Savings Account Menu ---------" << endl;
+        cout << endl << "--------- Savings Account Menu ---------" << endl;
         cout << "Enter 1 to deposit money." << endl;
         cout << "Enter 2 to withdraw money." << endl;
         cout << "Enter 3 to calculate interest for." << endl;
-        cout << "Enter 4 to exit" << endl;
+        cout << "Enter 4 to get account information." << endl;
+        cout << "Enter 5 to exit." << endl;
 
         int choice;
         do{
-            cout << "Enter your choice: ";
+            cout << endl << "Enter your choice: ";
             cin >> choice;
             switch(choice){
                 case 1: {
@@ -101,24 +107,199 @@ public:
                 }
 
                 case 3:
-                    cout << "The interest rate of Savings Account is " << interestRate << "/" << (100 * interestRate) << "%" << endl;
+                    cout << "The interest rate of Savings Account is " << interestRate << "(" << (100 * interestRate) << "%)" << endl;
                     cout << "Interest Amount is: $" << calculateInterest() << endl;
                     break;
 
                 case 4:
-                    cout << "Exiting savings account..." << endl;
+                    getAccountInfo();
+                    break;
+
+                case 5:
+                    cout << "Exiting Savings Account..." << endl;
                     break;
                 
                 default:
-                    cout << "Invalid Input, try again." << endl;
+                    cout << "Invalid input, try again!" << endl;
+            }
+        } while(choice != 5);
+    }
+};
+
+class CheckingAccount : public Account{
+public:
+    CheckingAccount(string num, double bal, string name, string type = "Checking Account") : Account(num, bal, name, type) {}
+
+    void printStatement() override {
+        cout << endl << "--------- Checking Account Menu ---------" << endl;
+        cout << "Enter 1 to deposit money." << endl;
+        cout << "Enter 2 to withdraw money." << endl;
+        cout << "Enter 3 to get account information." << endl;
+        cout << "Enter 4 to exit." << endl;
+
+        int choice;
+        do{
+            cout << endl << "Enter your choice: ";
+            cin >> choice;
+            switch(choice){
+                case 1: {
+                    double amount;
+                    cout << "Enter amount to deposit: ";
+                    cin >> amount;
+                    deposit(amount);
+                    break;
+                }
+
+                case 2: {
+                    double amount;
+                    cout << "Enter amount to withdraw: ";
+                    cin >> amount;
+                    withdraw(amount);
+                    break;
+                }
+
+                case 3:
+                    getAccountInfo();
+                    break;
+
+                case 4:
+                    cout << "Exiting Checking Account..." << endl;
+                    break;
+                
+                default:
+                    cout << "Invalid input, try again!" << endl;
             }
         } while(choice != 4);
-        getAccountInfo();
+    }    
+};
+
+class FixedDepositAccount : public Account{
+    string maturityDate;
+    const double fixedInterestRate;
+
+public:
+    FixedDepositAccount(string date, double rate, string num, double bal, string name, string type = "Fixed Deposit Account") :  maturityDate(date), fixedInterestRate(rate), Account(num, bal, name, type) {}
+
+    void withdraw(double amount) override {
+        string currentDate;
+        cout << "Enter today's date: ";
+        cin >> currentDate;
+
+        if(currentDate == maturityDate){
+            Account :: withdraw(amount);
+        }
+        else{
+            cout << "Cannot withdraw amount before maturity date from a Fixed Deposit Account." << endl;
+        }
+    }
+
+    double calculateInterest() override {
+        return (balance * fixedInterestRate);
+    }
+
+    void printStatement() override {
+        cout << endl << "--------- Fixed Deposit Account Menu ---------" << endl;
+        cout << "Enter 1 to deposit money." << endl;
+        cout << "Enter 2 to withdraw money." << endl;
+        cout << "Enter 3 to calculate interest for." << endl;
+        cout << "Enter 4 to get account information." << endl;
+        cout << "Enter 5 to exit." << endl;
+
+        int choice;
+        do{
+            cout << endl << "Enter your choice: ";
+            cin >> choice;
+            switch(choice){
+                case 1: {
+                    double amount;
+                    cout << "Enter amount to deposit: ";
+                    cin >> amount;
+                    deposit(amount);
+                    break;
+                }
+
+                case 2: {
+                    double amount;
+                    cout << "Enter amount to withdraw: ";
+                    cin >> amount;
+                    withdraw(amount);
+                    break;
+                }
+
+                case 3:
+                    cout << "The fixed interest rate of Fixed Deposit Account is " << fixedInterestRate << "(" << (100 * fixedInterestRate) << "%)" << endl;
+                    cout << "Interest Amount is: $" << calculateInterest() << endl;
+                    break;
+
+                case 4:
+                    getAccountInfo();
+                    break;
+
+                case 5:
+                    cout << "Exiting Fixed Deposit Account..." << endl;
+                    break;
+                
+                default:
+                    cout << "Invalid input, try again!" << endl;
+            }
+        } while(choice != 5);
     }
 };
 
 int main(){
-    Account *a = new SavingsAccount(0.10, 500, "24K-0687", 200 , "Rayyan Aamir", "Savings");
-    a->printStatement();
+    Account *a = nullptr;
+    string name, number;
+    double bal;
+    cout << "-------------- Welcome to Account System --------------" << endl;
+    cout << "Enter your account number: ";
+    getline(cin, number);
+    cout << "Enter your account holder name: ";
+    getline(cin, name);
+    cout << "Enter your account balance: ";
+    cin >> bal;
+    cin.ignore();
+    
+    char input;
+    cout << "Enter your account type ('S' for Savings Account, 'C' for Checking Account, 'F' for Fixed Deposit Account): ";
+    cin >> input;
+    switch(input){
+        case 'S':
+        case 's':
+        {
+            double minBal, rate;
+            cout << "Enter the minimum balance of your Savings Account: ";
+            cin >> minBal;
+            cout << "Enter the interest rate of your Savings Account: ";
+            cin >> rate;
+            a = new SavingsAccount(rate, minBal, number, bal, name);
+            break;
+        }
+
+        case 'C':
+        case 'c':
+            a = new CheckingAccount(number, bal, name);
+            break;
+
+        case 'F':
+        case 'f':
+        {
+            double rate;
+            string mDate;
+            cout << "Enter the maturity date of your Fixed Deposit Account: ";
+            cin >> mDate;
+            cout << "Enter the fixed interest rate of your Fixed Deposit Account: ";
+            cin >> rate;
+            a = new FixedDepositAccount(mDate, rate, number, bal, name);
+            break;
+        }
+
+        default:
+            cout << "Invalid input, try again!" << endl;
+    }
+
+    if(a != nullptr){
+        a->printStatement();
+    }
+    delete a;
     return 0;
 }
