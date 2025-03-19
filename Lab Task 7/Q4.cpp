dcc1a5be660fe4d705c84de89ffdb7812e2f844d
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-03-19 20:37:04 +0500

    Update Q4.cpp

--- Lab Task 7/Q4.cpp
+++ Lab Task 7/Q4.cpp
@@ -1,3 +1,15 @@
+/* Imagine designing a digital management system for a large university that seamlessly integrates academic and administrative operations. At the heart of this system is a Person
+class that stores universal data members such as name, id, address, phoneNumber, and email. It provides functions like displayInfo() to show personal details and updateInfo()
+to modify them.
+
+Derived classes—Student, Professor, and Staff—extend this system by introducing specific data members:
+Student includes coursesEnrolled, GPA, and enrollmentYear, modifying displayInfo() to show academic records.
+Professor has department, coursesTaught, and salary, updating displayInfo() to display faculty-specific details.
+Staff maintains department, position, and salary, tailoring displayInfo() to reflect administrative roles.
+
+An additional Course class, with attributes like courseld, courseName, credits, instructor, and schedule, provides functions such as registerStudent(student) and
+calculateGrades(). This ensures an interactive system where function overrides enable dynamic role-based management. */
+
 #include <iostream>
 using namespace std;
 
