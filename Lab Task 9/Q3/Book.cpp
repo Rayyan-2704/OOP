d4b153941c3506ed9ee8290adb5819945ff04575
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-04-16 03:43:00 +0500

    Update Book.cpp

--- Lab Task 9/Q3/Book.cpp
+++ Lab Task 9/Q3/Book.cpp
@@ -3,17 +3,17 @@
 #include "Book.h"
 using namespace std;
 
-Book :: Book() {}
-Book :: Book(string t, string a, string isbn) : title(t), author(a), ISBN(isbn) {}
+Book::Book() {}
+Book::Book(string t, string a, string isbn) : title(t), author(a), ISBN(isbn) {}
 
-string Book :: getTitle() const {
+string Book::getTitle() const {
     return title;
 }
 
-string Book :: getAuthor() const {
+string Book::getAuthor() const {
     return author;
 }
 
-string Book :: getISBN() const {
+string Book::getISBN() const {
     return ISBN;
 }
